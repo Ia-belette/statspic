1f96532177fca4569a1d12dd8e4ddfc7ad6784de
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-07-10 20:44:00 +0200

    feat: add current month functionality

---
+++ lib/current-month.ts
@@ -0,0 +1,19 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const d = new Date();
+const currentMonth = months[d.getMonth()];
+
+export const CURRENT_MONTH = `${currentMonth}'s`;
