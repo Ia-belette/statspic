38755d09b9c842a51a96d94653aa4f3d01593f88
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-07-10 20:43:49 +0200

    refactor: update harmony template component styles

--- stories/harmony.stories.tsx
+++ stories/harmony.stories.tsx
@@ -1,5 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Harmony } from '@/components/templates/harmony';
+import { Harmony } from '@/components/templates/harmony/harmony';
+import {
+  TOP_ARTISTS,
+  TOP_ARTIST_POSTER,
+  TOP_SONGS,
+  TOP_TRACK_POSTER,
+} from '@/fake-data';
+import {} from '@/components/templates/harmony/styles';
 
 const meta = {
   title: 'Templates/Harmony',
@@ -8,11 +15,63 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    visual: {
+      description: 'Visual theme',
+      name: 'Visual',
+      control: {
+        type: 'select',
+        labels: {
+          midgnights: 'Midgnights',
+          'like-that': 'Like That',
+          gobalt: 'Gobalt',
+          minty: 'Minty',
+        },
+      },
+      options: ['midgnights', 'like-that', 'gobalt', 'minty'],
+    },
+    posterUrl: {
+      control: {
+        type: 'text',
+      },
+      name: 'Poster URL',
+      description: 'URL of the poster image',
+    },
+    topArtists: {
+      control: {
+        type: 'object',
+      },
+      name: 'Top Artists',
+      description: 'List of top artists',
+    },
+    topSongs: {
+      control: {
+        type: 'object',
+      },
+      name: 'Top Songs',
+      description: 'List of top songs',
+    },
+  },
 } satisfies Meta<typeof Harmony>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const WithTopArtistPoster: Story = {
+  args: {
+    visual: 'midgnights',
+    topArtists: TOP_ARTISTS,
+    topSongs: TOP_SONGS,
+    posterUrl: TOP_ARTIST_POSTER,
+  },
+} satisfies Story;
+
+export const WithTopTrackPoster: Story = {
+  args: {
+    visual: 'midgnights',
+    topArtists: TOP_ARTISTS,
+    topSongs: TOP_SONGS,
+    posterUrl: TOP_TRACK_POSTER,
+  },
+} satisfies Story;
