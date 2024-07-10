42c3331abf7ce0bf2d3f06290fd170f3b19ed6ba
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-07-10 20:42:39 +0200

    chore: update panda.config.ts with new color tokens

--- panda.config.ts
+++ panda.config.ts
@@ -16,9 +16,28 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          midnights: { value: '#503658' },
+          'midnights-title': { value: '#E5AB8A' },
+          'midnights-text': { value: '#CFF469' },
+          'like-that': { value: '#000000' },
+          'like-that-title': { value: '#E91229' },
+          'like-that-text': { value: '#F8D4D7' },
+          gobalt: { value: '#3C00E4' },
+          'gobalt-title': { value: '#C0AFD0' },
+          'gobalt-text': { value: '#9BEFE2' },
+          minty: { value: '#ABEDBB' },
+          'minty-title': { value: '#22306D' },
+          'minty-text': { value: '#000000' },
+        },
+      },
+    },
   },
-
+  staticCss: {
+    recipes: '*',
+  },
   // The output directory for your css system
   outdir: 'styled-system',
 });
