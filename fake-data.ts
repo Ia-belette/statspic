addc6ee5702a5841d2fe73b8f1a8d5221f625918
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-07-10 17:14:14 +0200

    feat: add top artist and top track data to fake-data.ts

---
+++ fake-data.ts
@@ -0,0 +1,21 @@
+export const TOP_ARTIST_POSTER =
+  'https://i.scdn.co/image/ab67616d00001e02318443aab3531a0558e79a4d';
+
+export const TOP_TRACK_POSTER =
+  'https://i.scdn.co/image/ab67616d00001e02d9194aa18fa4c9362b47464f';
+
+export const TOP_ARTISTS = [
+  'Taylor Swift',
+  'Myd',
+  'Weval',
+  'Le Wanski',
+  'LCD Soundsystem',
+];
+
+export const TOP_SONGS = [
+  'Runaway',
+  'Into the Light',
+  'Snow',
+  'Hello',
+  'Hey Brother',
+];
