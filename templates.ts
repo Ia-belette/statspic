bce1609a97cf4f8a700beafded4f6d07dad1274c
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-07-12 22:18:27 +0200

    refactor: update template data structure and add template sections

--- templates.ts
+++ templates.ts
@@ -1,24 +1,58 @@
-import { Harmony } from './components/templates/harmony';
-import { Melody } from './components/templates/melody';
-import { Symphony } from './components/templates/symphony';
+import React, { lazy } from 'react';
 
-export const templates = [
-  {
+const Harmony = lazy(() => import('@/components/templates/harmony'));
+const Melody = lazy(() => import('@/components/templates/melody'));
+const Symphony = lazy(() => import('@/components/templates/symphony'));
+
+export interface Template {
+  id: number;
+  name: string;
+  shape: Shape;
+  component: React.ComponentType<any>;
+  colors: string[];
+}
+
+export enum Shape {
+  RECTANGLE = 13,
+  SQUARE = 4,
+}
+
+export const templatesData: Record<number, Template> = {
+  1: {
     id: 1,
     name: 'Harmony',
-    shape: 13, // vertical rectangle
+    shape: Shape.RECTANGLE,
     component: Harmony,
+    colors: ['midgnight', 'black', 'gobalt', 'minty'],
   },
-  {
+  2: {
     id: 2,
     name: 'Melody',
-    shape: 4, // square
+    shape: Shape.SQUARE,
     component: Melody,
+    colors: ['crimson', 'sky', 'yellow'],
   },
-  {
+  3: {
     id: 3,
     name: 'Symphony',
-    shape: 13, // vertical rectangle
+    shape: Shape.RECTANGLE,
     component: Symphony,
+    colors: ['lime', 'tomato'],
+  },
+};
+
+export interface TemplateSection {
+  section: string;
+  templates: number[];
+}
+
+export const templatesSections: TemplateSection[] = [
+  {
+    section: 'Vertical rectangle',
+    templates: [1, 3],
+  },
+  {
+    section: 'Card square',
+    templates: [2],
   },
 ];
