3988ab3265ff5e1d5ab1a86baee24eac9296fc22
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-07-12 15:20:02 +0200

    feat: add WhatIsThis and DotGrid components to me page

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,6 +1,8 @@
 import { auth } from '@/lib/auth/auth';
 import { Zoom } from '@/components/zoom';
 import { redirect } from 'next/navigation';
+import { WhatIsThis } from '@/components/what-is-this';
+import { DotGrid } from '@/components/dot-grid';
 
 const Page = async () => {
   const user = await auth();
@@ -12,7 +14,8 @@ const Page = async () => {
   return (
     <main className='min-h-screen grid grid-rows-10 md:grid-rows-none md:grid-cols-12'>
       <div className='relative row-span-6 md:row-span-12 md:col-span-8 flex justify-center items-center text-xl bg-[--gray-2]'>
-        <div className='dot'></div>
+        <WhatIsThis />
+        <DotGrid />
         <Zoom>
           <div className='!w-[360px] !h-[300px] bg-red-500'></div>
         </Zoom>
