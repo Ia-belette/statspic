44d3eefdc263bb7393e35be5cfb6a6df654962ac
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-07-12 14:58:55 +0200

    refactor: optimize zoom component for better performance

--- components/zoom.tsx
+++ components/zoom.tsx
@@ -6,30 +6,34 @@ import { useInitialPosition } from '@/hooks/use-position';
 export const Zoom = ({ children }: { children: React.ReactNode }) => {
   const wrapperRef = useRef(null);
   const transformWrapperRef = useRef(null);
+  const contentRef = useRef(null);
   const { initialPosition, wrapperSize } = useInitialPosition(
     wrapperRef,
-    transformWrapperRef
+    transformWrapperRef,
+    contentRef
   );
+
+  console.log('wraaaaaper 😤', wrapperSize);
 
   return (
     <div ref={wrapperRef} className='relative w-full h-full'>
-      {wrapperSize.width > 0 && wrapperSize.height > 0 && (
-        <TransformWrapper
-          ref={transformWrapperRef}
-          initialScale={0.8}
-          initialPositionX={initialPosition.x}
-          initialPositionY={initialPosition.y}
-          minScale={0.5}
-        >
-          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-            <React.Fragment>
-              <TransformComponent wrapperClass='!w-full !h-full'>
+      <TransformWrapper
+        ref={transformWrapperRef}
+        initialScale={0.8}
+        initialPositionX={initialPosition.x}
+        initialPositionY={initialPosition.y}
+        minScale={0.5}
+      >
+        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+          <React.Fragment>
+            <TransformComponent wrapperClass='!w-full !h-full'>
+              <div ref={contentRef} className='relative'>
                 {children}
-              </TransformComponent>
-            </React.Fragment>
-          )}
-        </TransformWrapper>
-      )}
+              </div>
+            </TransformComponent>
+          </React.Fragment>
+        )}
+      </TransformWrapper>
     </div>
   );
 };
