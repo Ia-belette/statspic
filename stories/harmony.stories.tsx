ea058f71fc3757cac3d6b4841df9cf44ce558852
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-07-12 17:33:26 +0200

    feat: add Harmony stories

---
+++ stories/harmony.stories.tsx
@@ -0,0 +1,85 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { HarmonyComponent as Harmony } from '@/components/templates/harmony';
+import {
+  TOP_ARTISTS,
+  TOP_ARTIST_POSTER,
+  TOP_SONGS,
+  TOP_TRACK_POSTER,
+} from '@/fake-data';
+
+const meta = {
+  title: 'Templates/Harmony',
+  component: Harmony,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    visual: {
+      description: 'Visual theme',
+      name: 'Visual',
+      control: {
+        type: 'select',
+        labels: {
+          midgnight: 'Midgnight',
+          black: 'Black',
+          gobalt: 'Gobalt',
+          minty: 'Minty',
+        },
+      },
+      options: ['midgnight', 'black', 'gobalt', 'minty'],
+    },
+    posterUrl: {
+      control: {
+        type: 'text',
+      },
+      name: 'Poster URL',
+      description: 'URL of the poster image',
+    },
+    topArtists: {
+      control: {
+        type: 'object',
+      },
+      name: 'Top Artists',
+      description: 'List of top artists',
+    },
+    topSongs: {
+      control: {
+        type: 'object',
+      },
+      name: 'Top Songs',
+      description: 'List of top songs',
+    },
+    moment: {
+      control: {
+        type: 'text',
+      },
+      name: 'Current Month',
+      description: 'Current month',
+    },
+  },
+} satisfies Meta<typeof Harmony>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const WithTopArtistPoster: Story = {
+  args: {
+    visual: 'midgnight',
+    topArtists: TOP_ARTISTS,
+    topSongs: TOP_SONGS,
+    posterUrl: TOP_ARTIST_POSTER,
+    moment: "JUNE'S",
+  },
+} satisfies Story;
+
+export const WithTopTrackPoster: Story = {
+  args: {
+    visual: 'black',
+    topArtists: TOP_ARTISTS,
+    topSongs: TOP_SONGS,
+    posterUrl: TOP_TRACK_POSTER,
+    moment: "JUNE'S",
+  },
+} satisfies Story;
